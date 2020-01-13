bd077224e340320bac60bee22081faa0ecf997b2
Miron Banks <madapegarage@gmail.com>
2020-01-13 16:04:58 +0100

    install husky

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import './App.css';
 
-const App = () => {
-	return <div className='App'>Hello</div>;
-};
+const App = () => <div className="App">Hello</div>;
 
 export default App;
