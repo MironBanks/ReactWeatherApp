c1ea20bc05f6bfbdaf94235d09ac1e1f82d9094c
Miron Banks <madapegarage@gmail.com>
2020-01-14 18:13:09 +0100

    function

--- src/App.js
+++ src/App.js
@@ -1,53 +1,90 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // eslint-disable-next-line no-unused-vars
 const api = {
   key: '9d6993d8acb3101b5c52469c91d2df70',
-  base: 'https:77api.openweathermap.org/data/2.5/',
+  base: 'https://api.openweathermap.org/data/2.5/',
 };
 
-const dateBuilder = d => {
-  const months = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December',
-  ];
+function App() {
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
 
-  const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  const search = evt => {
+    if (evt.key === 'Enter') {
+      // eslint-disable-next-line no-undef
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then(res => res.json())
+        .then(result => {
+          setWeather(result);
+          setQuery('');
+          console.log(result);
+        });
+    }
+  };
 
-  const day = days[d.getDay()];
-  const date = d.getDate();
-  const month = months[d.getMonth()];
-  const year = d.getFullYear();
+  const dateBuilder = d => {
+    const months = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December',
+    ];
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-  return `${day} ${date} ${month} ${year}`;
-};
+    const day = days[d.getDay()];
+    const date = d.getDate();
+    const month = months[d.getMonth()];
+    const year = d.getFullYear();
 
-const App = () => (
-  <div className="app">
-    <main>
-      <div className="search-box">
-        <input type="text" className="search-bar" placeholder="Search..." />
-      </div>
-      <div className="location-box">
-        <div className="location"> New York City, US</div>
-        <div className="date">{dateBuilder(new Date())}</div>
-      </div>
-      <div className="weather-box">
-        <div className="temp">15 &#8451; </div>
-        <div className="weather">Sunny</div>
-      </div>
-    </main>
-  </div>
-);
+    return `${day} ${date} ${month} ${year}`;
+  };
+
+  return (
+    <div
+      className={
+        // eslint-disable-next-line no-nested-ternary
+        typeof weather.main !== 'undefined' ? (weather.main.temp > 16 ? 'app warm' : 'app') : 'app'
+      }
+    >
+      <main>
+        <div className="search-box">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+            onChange={e => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
+          />
+        </div>
+        {typeof weather.main !== 'undefined' ? (
+          <div>
+            <div className="location-box">
+              <div className="location">
+                {weather.name}, {weather.sys.country}
+              </div>
+              <div className="date">{dateBuilder(new Date())}</div>
+            </div>
+            <div className="weather-box">
+              <div className="temp">{Math.round(weather.main.temp)}°c</div>
+              <div className="weather">{weather.weather[0].main}</div>
+            </div>
+          </div>
+        ) : (
+          ''
+        )}
+      </main>
+    </div>
+  );
+}
 
 export default App;
